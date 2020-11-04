e73db9bca3d0283112669c20623e1313a4986f38
Lee Isles <leeisles@gmail.com>
2020-11-04 11:51:08 -0500

    init

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    test: 'Initial Value in State'
   },
   mutations: {
+    UPDATE_TEST_STATE (state, value) {
+      state.test = value
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    testFromState: (state) => {return state.test}
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <h2>{{ testFromState }}</h2>
+
+    <!-- this component displays a button and will update state and re-render the text-->
+    <CoolComp />
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+import test from '@/mixins/test';
+import CoolComp from '@/components/CoolComp'
+
+export default {
+  name: 'admin',
+  mixins: [test],
+  components: {CoolComp},
+
+  // this watched state value and renders change in the template
+  computed: {
+    ...mapGetters([
+      'testFromState'
+    ]),
+
+   },
+
+}
+</script>
